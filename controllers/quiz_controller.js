7beb4d38e36f9b0906606562ac428e7b4109bcf7
Antonio Martínez González <antonio.martinez.glez@gmail.com>
2015-07-29 23:22:07 +0200

    Validación, edición y borrado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload factoriza el código si la ruta contiene :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
+  models.Quiz.findById(quizId).then(
     function(quiz) {
       if (quiz) {
         req.quiz = quiz;
@@ -21,18 +21,18 @@ exports.index = function(req, res) {
   if(req.query.search && req.query.search !== '') {
     var claves = percent + req.query.search.replace(' ',percent) + percent;
     models.Quiz.findAll({where: ["pregunta like ?", claves]}).then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
+      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     });
   } else {
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
+      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     });
   }
 };
 
 //GET /quizes/:id
 exports.show = function(req, res){
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/:id/answer
@@ -41,10 +41,59 @@ exports.answer = function(req, res){
     if(req.query.respuesta === req.quiz.respuesta) {
       resultado = 'Correcto';
     } 
-    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
+};
+
+//GET /quizes/new
+exports.new = function(req, res){
+  var quiz = models.Quiz.build( // crea objeto quiz
+			 {pregunta: 'Pregunta', respuesta: 'Respuesta'}
+  );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build(req.body.quiz);
+  quiz.validate().then( function(err) {
+    if (err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+      // save: guarda en DB campos pregunta y respuesta de quiz
+      quiz.save({fields: ["pregunta", "respuesta"]}).then( function(){ res.redirect('/quizes')}) 
+    }      // res.redirect: Redirección HTTP a lista de preguntas
+  });
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res){
+  var quiz = req.quiz;
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+  
+  req.quiz.validate().then( function(err) {
+    if (err) {
+      res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+    } else {
+      // save: guarda en DB campos pregunta y respuesta de quiz
+      req.quiz.save({fields: ["pregunta", "respuesta"]}).then( function(){ res.redirect('/quizes') }) 
+    }      // res.redirect: Redirección HTTP a lista de preguntas
+  });
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+  req.quiz.destroy().then( function() {
+      res.redirect('/quizes');
+  }).catch(function(error) {next(error)});
 };
 
 //GET /author
 exports.author = function(req, res){
-  res.render('author',{nombre: 'Antonio Martínez González', imagen: 'images/me.png'});
+  res.render('author',{nombre: 'Antonio Martínez González', imagen: 'images/me.png', errors: []});
 };
