cc0f0b22857f28cd2f9121df37dc0479f1c628ca
Antonio Martínez González <antonio.martinez.glez@gmail.com>
2015-08-06 13:22:10 +0200

    Publicación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comando con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 
 // Definición de rutas de sesion
@@ -30,8 +31,9 @@ router.put('/quizes/:quizId(\\d+)',       	sessionController.loginRequired, quiz
 router.delete('/quizes/:quizId(\\d+)',       	sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',	 	commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',	sessionController.loginRequired, commentController.publish);
 
 // Author
 router.get('/author',				quizController.author);
